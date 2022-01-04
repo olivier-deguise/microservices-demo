e2039f870238e71a8b57c7acfa0732250d8f26ab
Olivier De Guise <olivier.de-guise.1@ens.etsmtl.ca>
2022-01-04 14:10:23 -0500

    test

--- customer/src/main/java/com/org/myapp/demo/customer/CustomerApplication.java
+++ customer/src/main/java/com/org/myapp/demo/customer/CustomerApplication.java
@@ -2,17 +2,17 @@ package com.org.myapp.demo.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages = {"com.org.myapp.demo.customer", "com.org.myapp.demo.amqp"})
-//@EnableEurekaClient
 @EnableDiscoveryClient
+@EnableConfigurationProperties
 @EnableFeignClients(basePackages="com.org.myapp.demo.clients")
-@PropertySources(@PropertySource("classpath:clients-${spring.profiles.active}.properties"))
+//@PropertySources(@PropertySource("classpath:clients-${spring.profiles.active}.properties"))
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
